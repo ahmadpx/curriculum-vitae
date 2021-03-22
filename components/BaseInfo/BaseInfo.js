676af5e9bdeb4fe3a7455c4d49990524693df83f
Ahmed Abdallah <ahmadpx@hotmail.com>
2021-03-22 08:07:47 +0400

    base info

--- components/BaseInfo/BaseInfo.js
+++ components/BaseInfo/BaseInfo.js
@@ -1,99 +1,85 @@
 import styled from "styled-components";
 
-const Wrapper = styled.section`
-  .baseInfo {
-    &__avatar {
-      width: 120px;
-      height: 120px;
-      border-radius: 50%;
-      overflow: hidden;
-      margin: 0 auto 15px;
-      display: block;
-      border: 1px solid black;
-    }
+const Wrapper = styled.section``;
+const Avatar = styled.img`
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  overflow: hidden;
+  margin: 0 auto 15px;
+  display: block;
+  border: 1px solid black;
+`;
+const Name = styled.h1`
+  text-align: center;
+  margin: 0 auto 10px;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 1.4;
+  text-transform: uppercase;
+`;
+const JobTitle = styled.h2`
+  text-align: center;
+  margin: 0 auto 30px;
+  font-weight: 600;
+  font-size: 15px;
+`;
+const Separator = styled.div`
+  width: 25px;
+  height: 2px;
+  background: #000;
+  margin: 0 auto 30px;
+  display: block;
+`;
+const Meta = styled.ul`
+  list-style: none;
+  text-align: center;
+  padding: 0;
+  margin-bottom: 30px;
+`;
+const MetaItem = styled.li`
+  font-weight: 500;
+  font-size: 12px;
+  margin-bottom: 0;
+  text-align: center;
+`;
+const MetaLink = styled.a`
+  white-space: nowrap;
 
-    &__name {
-      text-align: center;
-      margin: 0 auto 10px;
-      font-size: 24px;
-      font-weight: 600;
-      line-height: 1.4;
-      text-transform: uppercase;
-    }
-
-    &__seperator {
-      display: none;
-      width: 25px;
-      height: 2px;
-      background: #000;
-      margin: 0 auto 30px;
-      display: block;
-    }
-
-    &__jobTitle {
-      text-align: center;
-      margin: 0 auto 30px;
-      font-weight: 600;
-      font-size: 15px;
-    }
-
-    &__meta {
-      list-style: none;
-      text-align: center;
-      padding: 0;
-      margin-bottom: 30px;
-    }
-
-    &__metaItem {
-      font-weight: 500;
-      font-size: 12px;
-      margin-bottom: 0;
-      text-align: center;
-    }
-
-    &__metaLink {
-      white-space: nowrap;
-
-      &,
-      &:visited,
-      &:active {
-        color: #04a0ca;
-        text-decoration: none;
-      }
-    }
+  &,
+  &:visited,
+  &:active {
+    color: #04a0ca;
+    text-decoration: none;
   }
 `;
 
 function BaseInfo() {
   return (
     <Wrapper>
-      <img
+      <Avatar
         src="//media-exp1.licdn.com/dms/image/C5603AQF-SSY0kL17FQ/profile-displayphoto-shrink_400_400/0/1547273969335?e=1622073600&v=beta&t=JYihBcs9bg43-MScCZJh4rC_pQSMmI8akORZyTEK5fM"
-        alt=""
-        className="baseInfo__avatar"
+        alt="Ahmed Abdallah"
       />
 
-      <h1 className="baseInfo__name">Ahmed Abdallah</h1>
+      <Name>Ahmed Abdallah</Name>
 
-      <h2 className="baseInfo__jobTitle">Senior Front End Engineer</h2>
+      <JobTitle>Senior Frontend Engineer</JobTitle>
 
-      <ul className="baseInfo__meta">
-        <li className="baseInfo__metaItem"> Egypt, EG</li>
-        <li className="baseInfo__metaItem"> +2 0111 94 88 558</li>
-        <li className="baseInfo__metaItem">
-          <a href="mailto:ahmadpx@hotmail.com" className="baseInfo__metaLink">
+      <Meta>
+        <MetaItem>Dubai, United Arab Emirates</MetaItem>
+        <MetaItem>+971 052 378 3204</MetaItem>
+        <MetaItem>
+          <MetaLink href="mailto:ahmadpx@hotmail.com">
             ahmadpx@hotmail.com
-          </a>
-        </li>
-        <li className="baseInfo__metaItem">
-          <a
-            href="https://www.linkedin.com/in/ahmed-abdallah-6047b644"
-            className="baseInfo__metaLink"
-          >
+          </MetaLink>
+        </MetaItem>
+        <MetaItem>
+          <MetaLink href="https://www.linkedin.com/in/ahmed-abdallah-6047b644">
             linkedin.com/in/ahmed-abdallah-6047b644
-          </a>
-        </li>
-      </ul>
+          </MetaLink>
+        </MetaItem>
+      </Meta>
     </Wrapper>
   );
 }
--- components/Experience/data.js
+++ components/Experience/data.js
@@ -13,13 +13,13 @@ export const experience = [
     ],
     titles: [
       {
-        title: "Front End Team Lead",
+        title: "Frontend Team Lead",
         period: "Jan 2020 – Present",
         responsibilities:
           "As Frontend Team Lead at Tajawal, I inspire day to day demonstration of Tajawal values. I'm willing to walk the talk & positively project the face of the brand, Pushing my team to be the best through daily short sessions about topics they need to develop on. and always discussing with the team members the best approaches to tackle each problem we face.",
       },
       {
-        title: "Senior Front End Engineer",
+        title: "Senior Frontend Engineer",
         period: "May 2017 – Jan 2020",
         responsibilities:
           "As Frontend engineer at Tajawal. I'm taking ownership for the main search form for flights/hotels. and delivering features for flights product as well with high quality standards and high sense to details regarding implementation, Sharing my knowledge with the team through sessions, articles and providing my feedback and suggestion to improve the overall workflow and development cycle.",
@@ -36,7 +36,7 @@ export const experience = [
     ],
     titles: [
       {
-        title: "Front End Engineer",
+        title: "Frontend Engineer",
         period: "Jul 2015 – May 2017",
         responsibilities:
           "As Frontend engineer at Jobzella i maintain the Frontend and enhance the user experience to meet the users and customers satisfaction using the cut edge front end technologies such as HTML5, CSS3, SASS, JavaScript, Responsive Design, jQuery, AngularJS, VueJS, Webpack, GulpJS and GIT.",
@@ -48,7 +48,7 @@ export const experience = [
     brands: [],
     titles: [
       {
-        title: "Front End Developer",
+        title: "Frontend Developer",
         period: "Jul 2011 – Jul 2015",
         responsibilities:
           "Working as Freelance has helped me to face a lot of problems which gained me a lot of experience, and pushing me to always learn to stuff to keep up.",
